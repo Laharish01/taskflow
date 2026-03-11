40094f8fa9e10b78699286d3a2cd170f88404e97
Laharish01 <laharish2k17@gmail.com>
2026-03-11 18:05:12 +0530

    git pages build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const base = process.env.VITE_BASE_PATH ?? '/'
-
 export default defineConfig({
   plugins: [vue()],
-  base,
+  base: "/taskflow/",
   build: {
     outDir: 'dist',
     rollupOptions: { output: { manualChunks: { vue: ['vue', 'pinia'] } } },
